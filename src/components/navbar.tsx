7aee291d2f646dddaf57991228393ac464eaeeb5
zachuri <punsalang.zachary@gmail.com>
2023-06-22 22:56:22 -0700

    navbar: changed prop: config-> mainNav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { MainNavItem } from "@/types"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
@@ -7,20 +8,16 @@ import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
-interface Config {
-  mainNav: { title: string; href: string }[]
-}
-
 interface Props {
   children: React.ReactNode
-  config: Config
+  mainNav: MainNavItem[]
 }
 
-export function Navbar({ children, config }: Props) {
+export function Navbar({ children, mainNav }: Props) {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={config.mainNav} />
+        <MainNav items={config} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">{children}</nav>
         </div>
