19b79d66f675dccf9d24e1945780d4ce22b309fc
zachuri <punsalang.zachary@gmail.com>
2023-06-23 03:20:23 -0700

    dashboard: added loading skeleton when loading a user

--- src/app/(control panel)/dashboard/page.tsx
+++ src/app/(control panel)/dashboard/page.tsx
@@ -3,14 +3,32 @@
 import React from "react"
 
 import { useUser } from "@/hooks/useUser"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+import { CardSkeleton } from "@/components/card-skeleton"
 
 export default function Page() {
   const user = useUser()
 
   return (
     <>
-      <div>Dashboard</div>
-      <pre>{user.user?.id}</pre>
+      {user.isLoading ? (
+        <>
+          <CardSkeleton />
+        </>
+      ) : (
+        <>
+          <Card>
+            <CardHeader>
+              <CardTitle>Dashboard</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <pre>{user.user?.id}</pre>
+              <pre>{user.user?.email}</pre>
+            </CardContent>
+          </Card>
+        </>
+      )}
     </>
   )
 }
