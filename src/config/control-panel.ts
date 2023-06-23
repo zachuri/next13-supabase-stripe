6f0857c00e8728182dca2290fccdc9e7da922177
zachuri <punsalang.zachary@gmail.com>
2023-06-22 22:57:34 -0700

    config/contro-panel: added mainNav and sideBar items

---
+++ src/config/control-panel.ts
@@ -0,0 +1,32 @@
+import { ControlPanelConfig } from "@/types"
+
+export const controlPanelConfig: ControlPanelConfig = {
+  mainNav: [
+    {
+      title: "Documentation",
+      href: "/docs",
+    },
+    {
+      title: "Support",
+      href: "/support",
+      disabled: true,
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Posts",
+      href: "/dashboard",
+      icon: "post",
+    },
+    {
+      title: "Billing",
+      href: "/dashboard/billing",
+      icon: "billing",
+    },
+    {
+      title: "Settings",
+      href: "/dashboard/settings",
+      icon: "settings",
+    },
+  ],
+}
