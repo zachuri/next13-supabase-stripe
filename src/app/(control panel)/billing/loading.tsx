aec1ec47d28349294817d23b0c69092ec0f405eb
zachuri <punsalang.zachary@gmail.com>
2023-06-24 22:53:26 -0700

    loading: added skeleton function within loading.tsx for billing & settings

--- src/app/(control panel)/billing/loading.tsx
+++ src/app/(control panel)/billing/loading.tsx
@@ -1,6 +1,29 @@
-import { CardSkeleton } from "@/components/card-skeleton"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
 import { DashboardHeader } from "@/components/header"
 import { DashboardShell } from "@/components/shell"
+
+function CardSkeleton() {
+  return (
+    <Card>
+      <CardHeader className="gap-2">
+        <Skeleton className="h-9 w-1/5" />
+        <Skeleton className="h-4 w-2/5" />
+      </CardHeader>
+      <CardContent>
+        <Card className="w-96 max-sm:w-60">
+          <CardHeader className="gap-2">
+            <Skeleton className="h-9 w-2/5" />
+            <Skeleton className="h-4 w-4/5" />
+          </CardHeader>
+          <CardFooter>
+            <Skeleton className="h-10 w-[120px]" />
+          </CardFooter>
+        </Card>
+      </CardContent>
+    </Card>
+  )
+}
 
 export default function BillingLoading() {
   return (
--- src/app/(control panel)/settings/loading.tsx
+++ src/app/(control panel)/settings/loading.tsx
@@ -1,8 +1,37 @@
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
-import { CardSkeleton } from "@/components/card-skeleton"
 import { DashboardHeader } from "@/components/header"
 import { DashboardShell } from "@/components/shell"
+
+function CardSkeleton() {
+  return (
+    <Card>
+      <CardHeader className="gap-3">
+        <Skeleton className="h-10 w-1/5" />
+        <Skeleton className="h-44 w-44 rounded-full sm:h-60 sm:w-60" />
+      </CardHeader>
+      <CardHeader className="gap-3">
+        <Skeleton className="h-10 w-1/5" />
+        <Skeleton className="h-4 w-3/5" />
+        <Skeleton className="h-10 w-2/5" />
+      </CardHeader>
+      <CardHeader className="gap-3">
+        <Skeleton className="h-10 w-1/5" />
+        <Skeleton className="h-4 w-3/5" />
+        <Skeleton className="h-10 w-2/5" />
+      </CardHeader>
+      <CardHeader className="gap-3">
+        <Skeleton className="h-10 w-1/5" />
+        <Skeleton className="h-4 w-3/5" />
+        <Skeleton className="h-10 w-2/5" />
+      </CardHeader>
+      <CardContent className="gap-3" />
+      <CardFooter>
+        <Skeleton className="h-8 w-[120px]" />
+      </CardFooter>
+    </Card>
+  )
+}
 
 export default function SettingsLoading() {
   return (
@@ -12,31 +41,7 @@ export default function SettingsLoading() {
         text="Manage account and website settings."
       />
       <div className="grid gap-10">
-        <Card>
-          <CardHeader className="gap-3">
-            <Skeleton className="h-10 w-1/5" />
-            <Skeleton className="h-44 w-44 rounded-full sm:h-60 sm:w-60" />
-          </CardHeader>
-          <CardHeader className="gap-3">
-            <Skeleton className="h-10 w-1/5" />
-            <Skeleton className="h-4 w-3/5" />
-            <Skeleton className="h-10 w-2/5" />
-          </CardHeader>
-          <CardHeader className="gap-3">
-            <Skeleton className="h-10 w-1/5" />
-            <Skeleton className="h-4 w-3/5" />
-            <Skeleton className="h-10 w-2/5" />
-          </CardHeader>
-          <CardHeader className="gap-3">
-            <Skeleton className="h-10 w-1/5" />
-            <Skeleton className="h-4 w-3/5" />
-            <Skeleton className="h-10 w-2/5" />
-          </CardHeader>
-          <CardContent className="gap-3" />
-          <CardFooter>
-            <Skeleton className="h-8 w-[120px]" />
-          </CardFooter>
-        </Card>
+        <CardSkeleton />
       </div>
     </DashboardShell>
   )
